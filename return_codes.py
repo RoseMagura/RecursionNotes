718bfc22268c2b73a471cbad76624c60576eda5f
RoseMagura <magurarm@guilford.edu>
2020-11-17 14:00:43 -0500

    Feat: Add Udacity solution

--- return_codes.py
+++ return_codes.py
@@ -1,23 +1,71 @@
-asciiDict = {(i - 96): chr(i) for i in range(97, 122)}
+def get_alphabet(number):
+    """
+    Helper function to figure out alphabet of a particular number
+    Remember: 
+        * ASCII for lower case 'a' = 97
+        * chr(num) returns ASCII character for a number e.g. chr(65) ==> 'A'
+    """
+    return chr(number + 96)
+
 final = []
-def all_codes(number, x, final):
-    if (number // x) == 0:
-        return 
+def all_codes(number):
+    if number == 0:
+        return ['']
+    # Get two right-most digits
+    remainder = number % 100
+    output_100 = list()
+    if remainder <= 26 and number > 9:
+        # get all codes for the remaining number
+        output_100 = all_codes(number // 100)
+        alphabet = get_alphabet(remainder)
 
-    if(26 > number % x > 0):
-        entry = asciiDict.get(number // x, '') + asciiDict.get(number % x, '')
-        print(entry)
-        final.append(entry)
+        for index, element in enumerate(output_100):
+            output_100[index] = element + alphabet
+    
+    # calculation for right-most digit
+    remainder = number % 10
+    output_10 = all_codes(number // 10)
+    alphabet = get_alphabet(remainder)
 
-    elif(number // x > 1):
-        letters = []
-        for digit in str(number // x):
-            letters.append(asciiDict.get(int(digit), ''))
-        final.append(''.join(letters))
-        print(letters)
-    all_codes(number, x * 10, final)
-    return
+    for index, element in enumerate(output_10):
+        output_10[index] = element + alphabet
+        
+    output = list()
+    output.extend(output_100)
+    output.extend(output_10)
+    
+    return output
 
+def test_function(test_case):
+    number = test_case[0]
+    solution = test_case[1]
+    
+    output = all_codes(number)
+    
+    output.sort()
+    solution.sort()
+    
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
 
-print(all_codes(4545, 1, final))
-print('final', str(final))
+number = 123
+solution = ['abc', 'aw', 'lc']
+test_case = [number, solution]
+test_function(test_case)
+
+number = 145
+solution =  ['ade', 'ne']
+test_case = [number, solution]
+test_function(test_case)
+
+number = 1145
+solution =  ['aade', 'ane', 'kde']
+test_case = [number, solution]
+test_function(test_case)
+
+number = 4545
+solution = ['dede']
+test_case = [number, solution]
+test_function(test_case)

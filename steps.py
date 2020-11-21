a536e2282012286582ad6385cdeed936d6231637
RoseMagura <magurarm@guilford.edu>
2020-11-20 19:47:50 -0500

    Feat: Steps solution

---
+++ steps.py
@@ -0,0 +1,20 @@
+"""
+param: n - number of steps in the staircase
+Return number of possible ways in which you can climb the staircase
+"""
+def staircase(n):
+    '''Hint'''
+    # Base Case - What holds true for minimum steps possible i.e.,
+    # n == 0, 1, 2 or 3? Return the number of ways the child 
+    # can climb n steps.
+    if n <= 0:
+        return 1
+
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4   
+    # Recursive Step - Split the solution into base case if n > 3.
+    return staircase(n - 1) + staircase(n - 2) + staircase(n - 3)
